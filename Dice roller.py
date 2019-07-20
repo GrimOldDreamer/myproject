001fd0408883ce446c6dc416ca37e077312d6af3
Volodymyr <grimolddreamer@gmail.com>
2019-07-20 12:51:09 +0300

    Tabletop game

---
+++ Dice roller.py
@@ -0,0 +1,29 @@
+#Dice roller
+
+#Modules
+import random
+
+#answer = input("Do you wanna make a roll?  y/n")
+
+#variables
+qt = int(input("How many Dices do you rolling\n"))
+d = int(input("Enter your Dice type\n"))
+i = 0
+roll = 0
+summ = 0
+answer = 'y'
+
+while answer == 'y':
+    while qt != i:
+        roll = random.randint(1,6)
+        print("You rolling : ",roll)
+        i+=1
+        summ +=roll
+    print("Summary is : ", summ)
+    answer = input("\nDo you wanna make a roll?  y/n\n")
+    if answer == 'y':
+        i = 0
+        qt = int(input("How many Dices do you rolling\n"))
+        d = int(input("Enter your Dice type\n"))
+
+input("Press any key")
